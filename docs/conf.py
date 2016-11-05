f78edbdfc86acb0753f365d2c36527bb3f0ec58e
comp500 <comp500@users.noreply.github.com>
2016-11-05 20:46:44 +0000

    add default config

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from recommonmark.parser import CommonMarkParser
+
+extensions = []
+templates_path = ['/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx', 'templates', '_templates', '.templates']
+source_suffix = ['.rst', '.md']		
+source_parsers = {		
+            '.md': CommonMarkParser,		
+        }
+master_doc = 'index'
+project = u'ict.infra.link'
+copyright = u'2016'
+version = 'latest'
+release = 'latest'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+htmlhelp_basename = 'ict-igcse-infralink'
+html_theme = 'sphinx_rtd_theme'
+file_insertion_enabled = False
+latex_documents = [
+  ('index', 'ict-igcse-infralink.tex', u'ict.infra.link Documentation',
+   u'', 'manual'),
+]
+
 html_static_path = ['_static']
 
 html_context = {
